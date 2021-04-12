c7bf54a6c8d08fe9347d6c0e99d8d5badd31e445
Owner <morningstar9047@gmail.com>
2021-04-12 17:43:58 +0900

    Memo Delete Scheduling

--- memo/src/main/kotlin/com/mistar/memo/MemoApplication.kt
+++ memo/src/main/kotlin/com/mistar/memo/MemoApplication.kt
@@ -2,8 +2,10 @@ package com.mistar.memo
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
+@EnableScheduling
 class MemoApplication
 
 fun main(args: Array<String>) {
--- memo/src/main/kotlin/com/mistar/memo/domain/model/repository/MemoRepository.kt
+++ memo/src/main/kotlin/com/mistar/memo/domain/model/repository/MemoRepository.kt
@@ -3,6 +3,9 @@ package com.mistar.memo.domain.model.repository
 import com.mistar.memo.domain.model.entity.Memo
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
+import java.time.LocalDateTime
 
 @Repository
-interface MemoRepository : JpaRepository<Memo, Int>
+interface MemoRepository : JpaRepository<Memo, Int> {
+    fun findAllByDeletedAtBeforeAndIsDeletedIsTrue(now: LocalDateTime): List<Memo>
+}
