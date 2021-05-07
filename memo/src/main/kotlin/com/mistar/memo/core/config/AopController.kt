35db64259a87b460bf3c69906fd67188315e8a8f
Joon Hee Song <jhsong@vitasoft.co.kr>
2021-05-07 17:31:02 +0900

    .

--- memo/src/main/kotlin/com/mistar/memo/core/config/AopController.kt
+++ memo/src/main/kotlin/com/mistar/memo/core/config/AopController.kt
@@ -1,9 +1,8 @@
 package com.mistar.memo.core.config
 
 import org.aspectj.lang.JoinPoint
-import org.aspectj.lang.annotation.Aspect
-import org.aspectj.lang.annotation.Before
-import org.aspectj.lang.annotation.Pointcut
+import org.aspectj.lang.ProceedingJoinPoint
+import org.aspectj.lang.annotation.*
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
 import java.time.LocalDateTime
@@ -15,14 +14,21 @@ class AopController {
 
     @Pointcut("@annotation(org.springframework.web.bind.annotation.GetMapping")
     fun getMapping() {
-        logger.info("${LocalDateTime.now()}")
-        logger.info("GetMapping called")
+        logger.info("${LocalDateTime.now()} 111 GetMapping called")
     }
 
     @Before("getMapping()")
     fun before(joinPoint: JoinPoint) {
-        logger.info("join point")
-        logger.info("Before logging start")
+        logger.info("${LocalDateTime.now()} 222 before called")
     }
 
+    @AfterReturning(pointcut = "getMapping()", returning = "result")
+    fun afterReturning(joinPoint: JoinPoint, result: Object) {
+        logger.info("${LocalDateTime.now()} 333 afterReturning called")
+    }
+
+    @Around("GetMapping()")
+    fun around(joinPoint: ProceedingJoinPoint):Object {
+
+    }
 }
