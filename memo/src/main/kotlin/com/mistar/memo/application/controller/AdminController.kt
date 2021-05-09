d51693650f2fe608c5496aa2d77d319a1d37e719
Owner <morningstar9047@gmail.com>
2021-05-09 16:47:56 +0900

    Feat: 특정 사용자의 모든 메모 조회

--- memo/src/main/kotlin/com/mistar/memo/application/controller/AdminController.kt
+++ memo/src/main/kotlin/com/mistar/memo/application/controller/AdminController.kt
@@ -1,7 +1,9 @@
 package com.mistar.memo.application.controller
 
 import com.mistar.memo.domain.model.dto.UserInfoDto
+import com.mistar.memo.domain.model.entity.Memo
 import com.mistar.memo.domain.service.AdminService
+import com.mistar.memo.domain.service.MemoService
 import io.swagger.annotations.ApiOperation
 import io.swagger.annotations.ApiResponse
 import io.swagger.annotations.ApiResponses
@@ -11,7 +13,8 @@ import org.springframework.web.bind.annotation.*
 @RestController
 @RequestMapping("/v1/admin")
 class AdminController(
-    private val adminService: AdminService
+    private val adminService: AdminService,
+    private val memoService: MemoService
 ) {
     @ApiOperation("사용자 목록 가져오기")
     @ApiResponses(
@@ -46,7 +49,7 @@ class AdminController(
 
     @ApiOperation("관리자 권한 부여")
     @ApiResponses(
-        ApiResponse(code = 204, message = "관리자 권한 부여 성공"),
+        ApiResponse(code = 200, message = "관리자 권한 부여 성공"),
         ApiResponse(code = 400, message = "잘못된 요청"),
         ApiResponse(code = 401, message = "인증 안됨"),
         ApiResponse(code = 403, message = "권한 없음"),
@@ -74,5 +77,22 @@ class AdminController(
         @PathVariable memoId: Int
     ) {
         return adminService.deleteMemo(memoId)
+    }
+
+    @ApiOperation("특정 사용자의 모든 메모 조회")
+    @ApiResponses(
+        ApiResponse(code = 200, message = "메모 조회 성공"),
+        ApiResponse(code = 400, message = "잘못된 요청"),
+        ApiResponse(code = 401, message = "인증 안됨"),
+        ApiResponse(code = 403, message = "권한 없음"),
+        ApiResponse(code = 404, message = "해당 id의 사용자 없음")
+    )
+    @GetMapping("/users/{userId}/memos/{page}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    fun getAllMemos(
+        @PathVariable userId: Int,
+        @PathVariable page: Int
+    ): List<Memo> {
+        return memoService.selectAllMemos(userId, page)
     }
 }
