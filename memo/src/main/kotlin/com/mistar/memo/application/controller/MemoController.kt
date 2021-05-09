9b7c527d96fda5c62eed645e699f26f93401690c
Owner <morningstar9047@gmail.com>
2021-05-09 16:38:25 +0900

    Feat: 특정 사용자의 메모 삭제

--- memo/src/main/kotlin/com/mistar/memo/application/controller/MemoController.kt
+++ memo/src/main/kotlin/com/mistar/memo/application/controller/MemoController.kt
@@ -8,8 +8,6 @@ import com.mistar.memo.domain.service.MemoService
 import io.swagger.annotations.ApiOperation
 import io.swagger.annotations.ApiResponse
 import io.swagger.annotations.ApiResponses
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
 
