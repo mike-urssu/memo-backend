20c27906def5276f54849f0e08f5c0bd78b7748d
Owner <morningstar9047@gmail.com>
2021-05-23 16:33:13 +0900

    Feat: 자신의 특정 메모 조회하기

--- memo/src/main/kotlin/com/mistar/memo/application/response/MemosResponse.kt
+++ memo/src/main/kotlin/com/mistar/memo/application/response/MemosResponse.kt
@@ -2,6 +2,6 @@ package com.mistar.memo.application.response
 
 import com.mistar.memo.domain.model.dto.MemoDto
 
-data class MemoResponse(
-    val memos: MemoDto
+data class MemosResponse(
+    val memos: List<MemoDto>
 )
