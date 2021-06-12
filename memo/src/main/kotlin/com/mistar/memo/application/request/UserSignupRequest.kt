b25e0a9ef0fe4efd14e08ae413049845929043ab
Owner <morningstar9047@gmail.com>
2021-06-12 23:42:04 +0900

    Feat: Signup Test

--- memo/src/main/kotlin/com/mistar/memo/application/request/UserSignupRequest.kt
+++ memo/src/main/kotlin/com/mistar/memo/application/request/UserSignupRequest.kt
@@ -4,8 +4,8 @@ import com.mistar.memo.core.utils.SHA256
 import com.mistar.memo.domain.model.dto.UserSignupDto
 
 class UserSignupRequest(
-    private val username: String,
-    private val password: String
+    val username: String,
+    val password: String
 ) {
     fun toUserSignupDto() = UserSignupDto(username, SHA256.encryptPassword(password))
 }
