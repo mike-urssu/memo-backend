18c3cbb016a7b0132993ee90b70b920ac608b2f5
Owner <morningstar9047@gmail.com>
2021-06-13 16:01:43 +0900

    Feat: SignOut Test

--- memo/src/main/kotlin/com/mistar/memo/application/request/UserSignOutRequest.kt
+++ memo/src/main/kotlin/com/mistar/memo/application/request/UserSignOutRequest.kt
@@ -3,9 +3,9 @@ package com.mistar.memo.application.request
 import com.fasterxml.jackson.annotation.JsonProperty
 import com.mistar.memo.domain.model.dto.UserSignOutDto
 
-class UserSignOutRequest(
+data class UserSignOutRequest(
     @JsonProperty("refreshToken")
-    private val refreshToken: String
+    val refreshToken: String
 ) {
     fun toUserSignOutDto() = UserSignOutDto(refreshToken)
 }
